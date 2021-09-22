46c4693a41860105ad5e79f862fb65e43f47c44a
rubenshibu <rubenshibu@gmail.com>
2021-09-22 14:38:28 +0530

    script

--- macChanger.py
+++ macChanger.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python
 
 import subprocess
-interface = input("Which one do you want to change \n [+] 1. wlan0 \n [+] 2. eth0")
+
+def change_Mac(interface, newMacAddress):
+  print("[=] changeing MAC Address for wlan0 to" + newMacAddress)
+  subprocess.call(["ifconfig", interface ,"down"],shell=True)
+  subprocess.call(["ifconfig wlan0 hw ether", newMacAddress], shell=True)
+  subprocess.call("ifconfig",interface, "up", shell=True)
+
+print("enter the Mac address you want to change (eg: 00:11:22:33:44:55) -")
+newMacAddress = int(input("New MAC >"))
+interface = input("Which one do you want to change \n [+] 1. wlan0 \n [+] 2. eth0 >")
 
 if (interface == 1):
-  newMacAddress = int(input("enter the Mac address you want to change (eg: 00:11:22:33:44:55) -"))
-  print("[=] changeing MAC Address for wlan0 to" + newMacAddress)
-  subprocess.call("ifconfig wlan0 down",shell=True)
-  subprocess.call("ifconfig wlan0 hw ether" + newMacAddress, shell=True)
-  subprocess.call("ifconfig wlan0 up", shell=True)
+  change_Mac(wlan0, newMacAddress)
 elif(interface == 2):
-  newMacAddress = int(input("enter the Mac address you want to change (eg: 00:11:22:33:44:55) -"))
-  print("[=] changeing MAC Address for eth0 to" + newMacAddress)
-  subprocess.call("ifconfig eth0 down",shell=True)
-  subprocess.call("ifconfig eth0 hw ether" + newMacAddress, shell=True)
-  subprocess.call("ifconfig eth0 up", shell=True)
+  change_Mac(eth0, newMacAddress)
 else:
   print("Error!!")
  
