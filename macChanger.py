247755948cc4151ba643cf9084421d5f07a75c31
rubenshibu <rubenshibu@gmail.com>
2021-09-22 12:21:03 +0530

    script add

--- macChanger.py
+++ macChanger.py
@@ -2,6 +2,9 @@
 
 import subprocess
 
-subprocess.call("ifconfig",shell=True)
+newMacAddress = int(input("enter the Mac address you want to change (eg: 00:11:22:33:44:55) -"))
 
+subprocess.call("ifconfig wlan0 down",shell=True)
+subprocess.call("ifconfig wlan0 hw ether" + newMacAddress, shell=True)
+subprocess.call("ifconfig wlan0 up", shell=True)
 
